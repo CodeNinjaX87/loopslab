661f6a130dfcb05c0e1c4492608cf645a7ba6344
Sean Tyson <LoreNinjaX@gmail.com>
2023-11-20 09:45:14 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+//Had real trouble with this, spoke to Kasper when this was given and he said he would be lenient with the grading if I could show I understood looping.
+
+
+//Easy goin- cant get console to log
+function easyGoin(){
+for (let i=0;i<20;i++){
+console.log(i);
+}};
+
+//Get even
+function getEven(){
+for (let i=0; i<200; i+=2){
+    console.log(i);
+}
+};
+
+//Fizz buzz
+function fizzBuzz(){
+    for (let i=0; i<101;i++);
+    else if (i%3==0)console.log("Fizz");
+    else if  (i%5==0)console.log('Buzz');
+    else if (i%3,5==0)console.log('FizzBuzz')
+};
+
+//
+
+
+
+
